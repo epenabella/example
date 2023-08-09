4235097e75df8a422a2ffef64f35ffb46d397a1d
Rick Penabella <ricalski@yahoo.com>
2023-08-09 14:56:04 +0200

    header is perfect

--- src/components/navigation/Header.tsx
+++ src/components/navigation/Header.tsx
@@ -1,5 +1,6 @@
 import Avatar from "../sub-components/avatar";
 import AvatarPic from '../../assets/images/avatar.webp';
+import './Header.scss';
 
 const Header = () => <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-transparent text-sm py-3 sm:py-0">
 <nav
@@ -15,30 +16,11 @@ const Header = () => <header className="flex flex-wrap sm:justify-start sm:flex-
         type="button"
         className="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-2 rounded-md font-medium text-black dark:text-white/[.5] shadow-sm align-middle hover:bg-white/[.1] dark:hover:text-white  transition-all text-sm"
         data-hs-collapse="#navbar-collapse-with-animation"
+
         aria-controls="navbar-collapse-with-animation"
         aria-label="Toggle navigation"
       >
-        <svg
-          className="hs-collapse-open:hidden w-4 h-4"
-          width={16}
-          height={16}
-          fill="currentColor"
-          viewBox="0 0 16 16"
-        >
-          <path
-            fillRule="evenodd"
-            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-          />
-        </svg>
-        <svg
-          className="hs-collapse-open:block hidden w-4 h-4"
-          width={16}
-          height={16}
-          fill="currentColor"
-          viewBox="0 0 16 16"
-        >
-          <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-        </svg>
+            <div id='animated-icon1' className="animated-icon1 navbar-collapse-toggle"><span className="bg-black dark:bg-white"></span><span className="bg-black dark:bg-white"></span><span className="bg-black dark:bg-white"></span></div>
       </button>
     </div>
   </div>
