a2d410cae3e682cc789e7f3cd6dd1dde998fb586
Rick Penabella <ricalski@yahoo.com>
2023-08-21 10:35:31 +0200

    added 3 more recommendations

--- src/Data/recommendations.ts
+++ src/Data/recommendations.ts
@@ -1,8 +1,32 @@
 import EvangeliImg from "../assets/images/evengeli.webp";
 import RickardImg from "../assets/images/rickard.webp";
 import GustafImg from "../assets/images/gustaf.webp";
+import Cecilia from "../assets/images/cecilia.webp";
+import Bivin from "../assets/images/bivin.webp";
+import Kelvin from "../assets/images/kelvin.webp";
 
 export const recommendations = [
+  {
+    name: "Cecilia Nilsson",
+    title: "Head of Product Sales Quote to Order @Volvo Group",
+    imgPath: Cecilia,
+    recommendation:
+      "Rick has been instrumental in the transformation journey we are on within Sales and Order products at Volvo Group IT. In addition to his sound technical skills and a fullstack profile Rick has a buzzing persona and will bring energy and passion into any team.",
+  },
+  {
+    name: "Bivin Easo",
+    title: "Digital Product Owner @Volvo Group",
+    imgPath: Bivin,
+    recommendation:
+      "Rick has been a true professional software engineer with a very positive attitude.  He was working in my team since last year and have been a great team player who is always willing to take new challenges. He has been really good with front end development and also worked on backend micro services. On top of the technical role he played the role of scrum master in our Agile Release  Train.He was very good in identifying requirements keeping an end user experience and adapted the feedbacks from them quickly. It will be great to have a fun loving team player like Rick in any organisation.",
+  },
+  {
+    name: "Kelvin Yorome",
+    title: "IT Architect @Volvo Group",
+    imgPath: Kelvin,
+    recommendation:
+      "I'm thrilled to recommend Rick for his outstanding expertise in frontend and backend development within the .NET framework, complemented by his growing proficiency in DevOps practices. Rick's social aptitude also stands out, enhancing his ability to collaborate effectively within cross-functional teams. His commitment to producing top-tier code, coupled with his friendly and approachable demeanor, makes him an asset that not only drives technical excellence but also fosters a positive and engaging work environment. I would be glad to work work with Rick in the future.",
+  },
   {
     name: "Evangeli Patsouri",
     title: "Software Developer @Ericsson",
